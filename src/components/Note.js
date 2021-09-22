b9b24bba6607bfb862ced5b0a37f84903f7c6168
michaelsatumba <80368334+michaelsatumba@users.noreply.github.com>
2021-09-22 11:14:41 -0700

    real

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,8 +3,8 @@ import React from "react";
 function Note() {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>title</h1>
+      <p>content</p>
     </div>
   );
 }
